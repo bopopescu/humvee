65a642cbfa1fe6446e2ea8f681a5051476618c18
underthepixel <underthepixel@gmail.com>
2018-07-26 21:59:43 +0900

    improve account post

--- app/controllers/account.go
+++ app/controllers/account.go
@@ -17,27 +17,28 @@ func (c Account) Get() revel.Result {
 
 	id := c.Params.Get("id")
 
-	var result string
+	var response string
 	var data1 string
 	err := app.DB.QueryRow("select data1 from account where id=?", id).Scan(&data1)
 	switch {
 	case err == sql.ErrNoRows:
-		result = "0"
+		response = "0"
 	case err != nil:
 		revel.ERROR.Println(err)
-		result = "fail"
+		response = "fail"
 	default:
-		result = data1
+		response = data1
 	}
 
-	return c.RenderText(result)
+	return c.RenderText(response)
 }
 
 func (c Account) Post() revel.Result {
 
 	values, err := c.Request.GetForm()
 	if err != nil {
-		revel.ERROR.Println(err)
+		revel.ERROR.Println("err != nil, err=", err)
+		return c.RenderText("fail")
 	}
 
 	id := values.Get("id")
@@ -46,17 +47,14 @@ func (c Account) Post() revel.Result {
 	query := "insert into account (id,data1) values (?,?) on duplicate key update data1=?"
 	result, err2 := app.DB.Exec(query, id, data, data)
 	if err2 != nil {
-		revel.ERROR.Println(err2)
+		revel.ERROR.Println("err2 != nil, err2=", err2)
+		return c.RenderText("fail")
 	}
 
-	_, err3 := result.LastInsertId()
+	_, err3 := result.RowsAffected()
 	if err3 != nil {
-		revel.ERROR.Println(err3)
-	}
-
-	_, err4 := result.RowsAffected()
-	if err4 != nil {
-		revel.ERROR.Println(err4)
+		revel.ERROR.Println("err3 != nil, err3=", err3)
+		return c.RenderText("fail")
 	}
 
 	return c.RenderText("success")
